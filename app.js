b5e51dd3af6c50a09a3dc4a23e7c683e4dbb5179
Leonardo Campos <leonardo.campos.galaz@gmail.com>
2025-08-05 18:50:09 -0400

    cambios curso

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-alert('Hola Mundo');
-let numeroUsuario = prompt("Me indicas el numero por favor:");
+//estas son las variables
+let numeroSecreto = 8;
+let numeroUsuario = prompt("Me indicas el numero entre 1 y 10 por favor:");
+
+console.log(numeroUsuario);
+/*
+Este codigo realiza
+ la comparación 
+*/
+
+if (numeroUsuario == numeroSecreto) {
+    alert(`acertaste, el numero es: ${numeroUsuario}`);
+ } else {
+    if (numeroUsuario > numeroSecreto) {
+        alert('el numero secreto es menor');
+    } else {   
+        alert('el numero secreto es mayor');
+    }
+
+
+    //alert('no acertaste el numero, intenta de nuevo carnalito');
+}
